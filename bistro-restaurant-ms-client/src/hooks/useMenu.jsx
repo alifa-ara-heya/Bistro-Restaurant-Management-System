998ee151b4a256a756542db38dbdebd9def7d730
Alifa Ara Heya <alifaaraheya@gmail.com>
2024-12-30 20:38:21 -0500

    added swiper slider

--- bistro-restaurant-ms-client/src/hooks/useMenu.jsx
+++ bistro-restaurant-ms-client/src/hooks/useMenu.jsx
@@ -3,9 +3,10 @@ import { useEffect, useState } from "react";
 const useMenu = () => {
     const [menu, setMenu] = useState([]);
     const [loading, setLoading] = useState(true)
-
+    // console.log(`${import.meta.env.VITE_API_URL}/menu`);
     useEffect(() => {
-        fetch('menu.json')
+        // fetch('menu.json')
+        fetch(`${import.meta.env.VITE_API_URL}/menu`)
             .then(res => res.json())
             .then(data => {
                 setMenu(data)
