27d357ea393827f6fabf689dfc38bc59525a7dfb
Alifa Ara Heya <alifaaraheya@gmail.com>
2024-12-30 19:40:19 -0500

    added dotenv gitignore

--- bistro-restaurant-ms-server/index.js
+++ bistro-restaurant-ms-server/index.js
@@ -1,2 +1,19 @@
 const express = require('express');
+const app = express();
+require('dotenv').config();
 const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('boss is here')
+})
+
+app.listen(port, () => {
+    console.log('Bistro boss is sitting on port', port);
+})
