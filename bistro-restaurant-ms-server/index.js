bcd656ab69ae714c0899801b5472676972566598
Alifa Ara Heya <alifaaraheya@gmail.com>
2025-01-11 00:04:44 -0500

    server deploy

--- bistro-restaurant-ms-server/index.js
+++ bistro-restaurant-ms-server/index.js
@@ -9,7 +9,17 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 // middleware
 app.use(cors());
-app.use(express.json());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://bistro-restaurant-alifa.web.app",
+            "bistro-restaurant-alifa.firebaseapp.com",
+        ],
+        credentials: true,
+    })
+);
+
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -321,9 +331,9 @@ async function run() {
         })
 
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
